efcb16c46ba30d6c1a7d0b0b7a00acfad4699b44
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-04-26 16:44:37 +0300

    Apply DIP by making service interfaces

---
+++ Services/IManagerServices.cs
@@ -0,0 +1,10 @@
+﻿using AirportTicketBookingSystem.Enums;
+using AirportTicketBookingSystem.Models;
+
+namespace AirportTicketBookingSystem.Services;
+
+public interface IManagerServices
+{
+    public List<Booking> FilterBookings(string? keyword = null, decimal? price = null, DateTime? date = null,
+        Class? flightClass = null, string? passenger = null);
+}
