af213decf64d1c51c3add09bd3128f1e7f3bcd05
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-04-27 22:08:58 +0300

    Finish login handling

--- Launchers/MenuHandler.cs
+++ Launchers/MenuHandler.cs
@@ -1,4 +1,6 @@
-﻿using AirportTicketBookingSystem.Printers;
+﻿using AirportTicketBookingSystem.Context;
+using AirportTicketBookingSystem.Enums;
+using AirportTicketBookingSystem.Printers;
 using AirportTicketBookingSystem.Utilities;
 
 namespace AirportTicketBookingSystem.launchers;
@@ -34,10 +36,43 @@ public static class MenuHandler
     
     private static void LoginHandler()
     {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("==== Login ====\n");
+            
+            var name = ScannerUtil.ScanNonEmptyString("your name: ");
+            var password = ScannerUtil.ScanNonEmptyString("your password: ");
+            
+            bool success = AppStartup.AuthController.Login(name, password);
+
+            if (success)
+            {
+                var userType = UserContext.GetCurrentUserType();
+                if (userType == UserType.Manager)
+                {
+                    ManagerMenuHandler();
+                }
+                else if (userType == UserType.Passenger)
+                {
+                    PassengerMenuHandler();
+                }
+            }
+            Thread.Sleep(4000);
+        }
+    }
+
+    private static void SignUpHandler()
+    {
         throw new NotImplementedException();
     }
     
-    private static void SignUpHandler()
+    private static void ManagerMenuHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void PassengerMenuHandler()
     {
         throw new NotImplementedException();
     }
