ee201cbbd770f707c618e7baf01c2f9923e89c01
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-04-27 11:52:21 +0300

    Create a new model for flight filter

---
+++ Models/FlightFilter.cs
@@ -0,0 +1,14 @@
+﻿using AirportTicketBookingSystem.Enums;
+
+namespace AirportTicketBookingSystem.Models;
+
+public class FlightFilter
+{
+    public decimal? Price { get; set; }
+    public string? DepartureCountry { get; set; }
+    public string? DestinationCountry { get; set; }
+    public DateTime? DepartureDate { get; set; }
+    public string? DepartureAirport { get; set; }
+    public string? DestinationAirport { get; set; }
+    public FlightClass? FlightClass { get; set; }
+}
