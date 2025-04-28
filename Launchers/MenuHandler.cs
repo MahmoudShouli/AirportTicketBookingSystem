4c3fcdabfb50d466e87d571cbf54f7d63bc519b8
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-04-28 17:40:01 +0300

    Make views

--- Launchers/MenuHandler.cs
+++ Launchers/MenuHandler.cs
@@ -2,6 +2,7 @@
 using AirportTicketBookingSystem.Enums;
 using AirportTicketBookingSystem.Printers;
 using AirportTicketBookingSystem.Utilities;
+using AirportTicketBookingSystem.Views;
 
 namespace AirportTicketBookingSystem.launchers;
 
@@ -18,10 +19,10 @@ public static class MenuHandler
             switch (choice)
             {
                 case 1:
-                    LoginHandler();
+                    AuthView.LoginHandler();
                     break;
                 case 2:
-                    RegisterHandler();
+                    AuthView.RegisterHandler();
                     break;
                 case 3:
                     HelperPrinter.PrintFinishMessage();
@@ -33,57 +34,7 @@ public static class MenuHandler
         }
     }
     
-    private static void LoginHandler()
-    {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("==== Login ====\n");
-            
-            var name = ScannerUtil.ScanNonEmptyString("your name: ");
-            var password = ScannerUtil.ScanNonEmptyString("your password: ");
-            
-            bool success = AppStartup.AuthController.Login(name, password);
-
-            if (success)
-            {
-                var userType = UserContext.GetCurrentUserType();
-                if (userType == UserType.Manager)
-                {
-                    ManagerMenuHandler();
-                    return;
-                }
-                else if (userType == UserType.Passenger)
-                {
-                    PassengerMenuHandler();
-                    return;
-                }
-            }
-            HelperPrinter.PrintAnyKeyMessage();
-        }
-    }
-
-    private static void RegisterHandler()
-    {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("==== Registration ====\n");
-            
-            var name = ScannerUtil.ScanNonEmptyString("your name: ");
-            var password = ScannerUtil.ScanNonEmptyString("your password: ");
-            
-            bool success = AppStartup.AuthController.Register(name, password);
-
-            if (success)
-            {
-                PassengerMenuHandler();
-            }
-            HelperPrinter.PrintAnyKeyMessage();
-        }
-    }
-    
-    private static void ManagerMenuHandler()
+    public static void ManagerMenuHandler()
     {
         while (true)
         {
@@ -97,10 +48,10 @@ public static class MenuHandler
                     MenuPrinter.PrintFilterBookingsMenu();
                     break;
                 case 2:
-                    ImportHandler();
+                    ManagerView.ImportHandler();
                     break;
                 case 3:
-                    LogoutHandler();
+                    AuthView.LogoutHandler();
                     return;
                 default:
                     HelperPrinter.PrintAnyKeyMessage("Invalid choice (must be 1 - 3)");
@@ -109,38 +60,8 @@ public static class MenuHandler
         }
     }
     
-    private static void ImportHandler()
-    {
-        while (true)
-        {
-            MenuPrinter.PrintValidationDetails();
-            
-            var input = ScannerUtil.ScanNonEmptyString("your file path or Q to exit: ");
-            
-            if (input.Equals("Q", StringComparison.OrdinalIgnoreCase))
-                return;
-            
-            var report = AppStartup.ManagerController.ImportFlights(input);
-
-            // 1 means the report only contains the header (no errors added to it)
-            if (report.Count == 1)
-            {
-                HelperPrinter.PrintAnyKeyMessage("Import success!");
-                return;
-            }
-            
-            report.ForEach(Console.WriteLine);
-            HelperPrinter.PrintAnyKeyMessage();
-        }
-    }
-
-    private static void PassengerMenuHandler()
+    public static void PassengerMenuHandler()
     {
         
-    }
-    
-    private static void LogoutHandler()
-    {
-        UserContext.ResetCurrentUser();
     }
 }
---
+++ Views/ManagerView.cs
@@ -0,0 +1,33 @@
+﻿using AirportTicketBookingSystem.launchers;
+using AirportTicketBookingSystem.Printers;
+using AirportTicketBookingSystem.Utilities;
+
+namespace AirportTicketBookingSystem.Views;
+
+public static class ManagerView
+{
+    public static void ImportHandler()
+    {
+        while (true)
+        {
+            MenuPrinter.PrintValidationDetails();
+            
+            var input = ScannerUtil.ScanNonEmptyString("your file path or Q to exit: ");
+            
+            if (input.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                return;
+            
+            var report = AppStartup.ManagerController.ImportFlights(input);
+
+            // 1 means the report only contains the header (no errors added to it)
+            if (report.Count == 1)
+            {
+                HelperPrinter.PrintAnyKeyMessage("Import success!");
+                return;
+            }
+            
+            report.ForEach(Console.WriteLine);
+            HelperPrinter.PrintAnyKeyMessage();
+        }
+    }
+}
